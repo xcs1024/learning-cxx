2a7ca87b381821493440a0a7e629f2557f1459e8
xcs1024 <xcs1005@gmail.com>
2024-12-23 19:28:48 +0800

    main

---
+++ exercises/11_method/main.cpp
@@ -0,0 +1,26 @@
+#include "../exercise.h"
+
+struct Fibonacci {
+    unsigned long long cache[128];
+    int cached;
+
+    // TODO: 实现正确的缓存优化斐波那契计算
+    unsigned long long get(int i) {
+        if (i < cached){
+            return cache[i];
+        }
+        for (; cached <= i; ++cached) {
+            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        }
+        cached = i + 1;
+        return cache[i];
+    }
+};
+
+int main(int argc, char **argv) {
+    // TODO: 初始化缓存结构体，使计算正确
+    Fibonacci fib = {{0, 1}, 2};
+    ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
+    std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
+    return 0;
+}
